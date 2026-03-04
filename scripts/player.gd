96de04d17e20b098cd7e38d4df739890755d26d2
lemendozar <54376099+lemendozaru@users.noreply.github.com>
2026-03-03 20:56:51 -0600

    Corrección de errores

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,23 +5,25 @@ signal hit
 @export var speed = 400
 var screen_size
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta):
 	var velocity = Vector2.ZERO # vector de movimiento del jugador
-	if Input.is_action_pressed("left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("right"):
+	if Input.is_action_pressed("derecha"):
 		velocity.x += 1
-	if Input.is_action_pressed("down"):
+	if Input.is_action_pressed("izquierda"):
+		velocity.x -= 1
+	if Input.is_action_pressed("abajo"):
 		velocity.y += 1
-	if Input.is_action_pressed("up"):
+	if Input.is_action_pressed("arriba"):
 		velocity.y -= 1
-	
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
@@ -41,9 +43,9 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D):
-	hide() # el nodo Player desaparece al ser golpeado
+	hide() # Player desaparece al ser golpeado
 	hit.emit() # se emite la señal de ser golpeado
-	# Debe apagarse la emisión de señal para que no se repita
+	# Debe apagarse la emisión de señal para que no se repita 
 	$CollisionShape2D.set_deferred("disabled", true)
 	
 func start(pos):
